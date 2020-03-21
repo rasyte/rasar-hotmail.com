170eb1ae4b69d2888562f85b2a6bf1f55d00bc1b
thurizas <thurizas123@gmail.com>
2020-03-21 16:04:24 -0400

    Add files via upload

--- common/common.h
+++ common/common.h
@@ -1,1 +1,8 @@
+#ifndef _common_h_
+#define _common_h_
 
+enum CMDS { CMD_HRT_BEAT = 0, CMD_UPDATE_STATE, CMD_SUGGEST, CMD_ACCUSE, CMD_INIT, CMD_PLAYER_JOIN, CMD_SHUTDOWN, CMD_HEARTBEAT };
+
+
+
+#endif
