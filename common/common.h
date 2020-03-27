17e4343df2344251c87a3a064ec395d22050a1db
thurizas <thurizas123@gmail.com>
2020-03-27 12:57:39 -0400

    Add files via upload

--- common/common.h
+++ common/common.h
@@ -1,12 +1,15 @@
 #ifndef _common_h_
 #define _common_h_
 
-enum CMDS { CMD_HRT_BEAT = 0, CMD_UPDATE_STATE, CMD_SUGGEST, CMD_ACCUSE, CMD_INIT, CMD_PLAYER_JOIN, CMD_SHUTDOWN, CMD_GAME_OVER};
+enum CMDS { CMD_HRT_BEAT = 0, CMD_UPDATE_STATE, CMD_SUGGEST, CMD_ACCUSE, CMD_INIT, CMD_PLAYER_JOIN, CMD_SHUTDOWN, 
+            CMD_GAME_BEGIN, CMD_GAME_SELECT, CMD_GAME_OVER};
 
 static const short HDR_LEN = 3;
+static const int NBR_SUSPECTS = 6;
 static const char* lpszSuspects[] = { "Col. Mustard","Prof. Plum","Mr. Green","Mrs. Peacock","Miss Scarlet", "Mrs. White" };
 static const char* lpszRooms[] = {"Hall", "Lounge", "Dining Room", "Kitchen", "Ball Room","Conservatory", "Billard Room", "Library", "Study"};
 static const char* lpszWeapons[] = {"Knife", "Candlestick", "Revolver", "Rope", "Lead Pipe", "Wrench"};
+
 
 #ifdef __WIN
 #pragma pack(show)
