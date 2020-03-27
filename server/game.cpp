556ed248f8438a84bb67f2c0c89444a4ad9480d0
thurizas <thurizas123@gmail.com>
2020-03-27 12:55:50 -0400

    Add files via upload

--- server/game.cpp
+++ server/game.cpp
@@ -1,44 +1,73 @@
 #include "manager.h"
+#include "../common/common.h"
 
 #include <vector>
 #include <iostream>
+#include <iterator>
+#include <algorithm>
+
+const char* szWelcomeMsg = "Welcome to Clue-less, game starting soon";
 
 void game(std::vector<pconnInfoT> vecPlayers)
 {
-  bool bWinner = false;
+    bool bWinner = false;
+    std::ostream_iterator<int> out_it(std::cout, ", ");
+    char  avatar[6] = { 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 };  // 0x00 - avatar is available, 0xFF avatar is not available
 
-  std::cout << "[game] nbr of players: " << vecPlayers.size() << std::endl;
+    std::cout << "[game] nbr of players: " << vecPlayers.size() << std::endl;
 
-  std::vector<pconnInfoT>::iterator    iter = vecPlayers.begin();
+    std::vector<pconnInfoT>::iterator    iter = vecPlayers.begin();
 
-  while(vecPlayers.end() != iter)
-  {
-    // TODO : sent welcome message
-    // TODO : send init message
-    
-    ++iter;
-  }
+    while(vecPlayers.end() != iter)
+    {
+        int fdClient = (*iter)->connfd; 
+        msgT  msg;
+            
+        // build welcome message...
+        std::cout << "[game] sending welcome message to player " << std::endl;
+        msg.msgLen = (short)(3 + strlen(szWelcomeMsg));
+        msg.chCode = CMD_GAME_BEGIN;
+        strcpy(msg.szMsg, szWelcomeMsg);
+            
+        send(fdClient, (const char*)&msg, msg.msgLen, 0);           // sent welcome message
 
-  // TODO : perform initialization
-  int player = 0;                  // should be the person that choose Miss Scarlet
+        // build avatar select message...
+        std::cout << "[game] sending select message to player " << std::endl;
+        std::cout << "[game] available avatars are: ";
+        std::copy(avatar, avatar + NBR_SUSPECTS, out_it);
+        msg.msgLen = 3 + NBR_SUSPECTS;
+        msg.chCode = CMD_GAME_SELECT;
+        memcpy(msg.szMsg, avatar, NBR_SUSPECTS);
+        send(fdClient, (const char*)&msg, msg.msgLen, 0);           // send init message
 
-  while(!bWinner)
-  {
-    // run turn for first player....
+        recv(fdClient, (char*)&msg, msg.msgLen, 0);                 // get response from the client
+        memcpy(avatar, msg.szMsg, NBR_SUSPECTS);                    // update the avatar array
+        std::cout << "[game] available avatars are: ";
+        std::copy(avatar, avatar + NBR_SUSPECTS, out_it);
+        std::cout << std::endl;
+        ++iter;
+    }
+
+    // TODO : perform initialization
+    int player = 0;                  // should be the person that choose Miss Scarlet
+
+    while(!bWinner)
+    {
+        // run turn for first player....
 
 
 
-    player = (player + 1) % vecPlayers.size();
-  }
+        player = (player + 1) % vecPlayers.size();
+    }
  
   
   
 
 
-  // TODO : send game closing message
-  // TODO : close all player sockets
+    // TODO : send game closing message
+    // TODO : close all player sockets
 
 
 
-  std::cout << "[game] bottom of game loop" << std::endl;
+    std::cout << "[game] bottom of game loop" << std::endl;
 }
--- server/manager.cpp
+++ server/manager.cpp
@@ -55,42 +55,42 @@ void manage()
        ret = timerfd_settime(fd, 0, &timeout, NULL);
        if (!ret)
        {
-	 while (g_bMgrRun)
-         {
-	   std::vector<pconnInfoT> players;
-	   //bool bBuilding = false;
+           while (g_bMgrRun)
+           {
+	            std::vector<pconnInfoT> players;
+	            //bool bBuilding = false;
 
 	   
-	   unsigned int nSize = g_conns.size();
+	            unsigned int nSize = g_conns.size();
 
-	   if(((nSize >= 3) && (nSize <= 6)) || g_bForce)
-	   {
-	     g_mutexque.lock();                                     // lock the queue
-	     for(int ndx = nSize-1; ndx >=0; ndx--)
-	     {
-	       std::cout << "[manager] Moving player: " << ndx << " to game" <<std::endl;
-	       players.push_back(g_conns.at(ndx));                  // move player from connections queue to players
-	       g_conns.erase(g_conns.begin() + ndx);                // remove the connection from the queue
-	     }
+	            if(((nSize >= 3) && (nSize <= 6)) || g_bForce)
+	            {
+	                g_mutexque.lock();                                     // lock the queue
+	                for(int ndx = nSize-1; ndx >=0; ndx--)
+	                {
+	                    std::cout << "[manager] Moving player: " << ndx << " to game" <<std::endl;
+	                    players.push_back(g_conns.at(ndx));                  // move player from connections queue to players
+	                    g_conns.erase(g_conns.begin() + ndx);                // remove the connection from the queue
+	                }  
 
-	     g_conns.erase(g_conns.begin(), g_conns.end());
-	     g_bForce = false;
+	                g_conns.erase(g_conns.begin(), g_conns.end());
+	                g_bForce = false;
 
-	     g_mutexque.unlock();
-	     std::cout << "[manager] Spawning a new game" << std::endl;
-	     std::thread  temp(game, players);
-	     temp.detach();                                    // detach from the game thread
+	                g_mutexque.unlock();
+	                std::cout << "[manager] Spawning a new game" << std::endl;
+	                std::thread  temp(game, players);
+	                temp.detach();                                    // detach from the game thread
 	   
 	     
-	   }
+	            }
 	   
-	     sleep(5);
+	            sleep(5);
 	
-	 }  // end of while loop
+	       }  // end of while loop
        }    // end of 'if(!ret)' block
        else
        {
-	 std::cerr << "[manager] falied to set timer duration, error is " << errno << std::endl;
+	       std::cerr << "[manager] falied to set timer duration, error is " << errno << std::endl;
        }
      }
      else
